993e7abdc2a898c466e269e51c9fa5a6da2f9ff1
root-not-sudo <brent.markus@gmail.com>
2024-12-23 14:46:32 -0800

    added circleshape and player, can turn player

--- main.py
+++ main.py
@@ -3,15 +3,13 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -19,7 +17,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0,0,0))
+        screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
+
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
